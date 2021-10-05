53d249b9021e1ff141f71253a152159d77fbbb1d
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-05 10:35:59 +0300

    Update ArrayCreatorImplTest.java

--- src/test/java/by/epam/task1/creator/impl/ArrayCreatorImplTest.java
+++ src/test/java/by/epam/task1/creator/impl/ArrayCreatorImplTest.java
@@ -3,6 +3,7 @@ package by.epam.task1.creator.impl;
 import by.epam.task1.creator.ArrayCreator;
 import by.epam.task1.entity.CustomArray;
 
+import by.epam.task1.exception.CustomArrayException;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -10,9 +11,10 @@ import static org.testng.Assert.*;
 public class ArrayCreatorImplTest {
 
     @Test
-    public void factoryArrayTest() {
+    public void factoryArrayTest() throws CustomArrayException {
         ArrayCreator arrayCreator = new ArrayCreatorImpl();
-        CustomArray actual = arrayCreator.factoryArray(new int[]{23, 35, 26});
+        int[] array = new int[0];
+        CustomArray actual = arrayCreator.createCustomArray(array);
         CustomArray expected = new CustomArray(new int[]{23, 35, 26});
         assertEquals(actual, expected);
     }
