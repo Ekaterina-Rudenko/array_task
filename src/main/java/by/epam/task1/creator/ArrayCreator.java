2a3ed1c8ea0c469145456b4c86cd7bc2557efde3
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-05 10:37:14 +0300

    Update ArrayCreator.java

--- src/main/java/by/epam/task1/creator/ArrayCreator.java
+++ src/main/java/by/epam/task1/creator/ArrayCreator.java
@@ -1,7 +1,8 @@
 package by.epam.task1.creator;
 
 import by.epam.task1.entity.CustomArray;
+import by.epam.task1.exception.CustomArrayException;
 
 public interface ArrayCreator {
-    public CustomArray factoryArray(int[] numberArray);
+    public CustomArray createCustomArray(int[] numberArray) throws CustomArrayException;
 }
