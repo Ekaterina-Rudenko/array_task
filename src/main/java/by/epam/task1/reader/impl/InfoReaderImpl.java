02ed7460c56686709e772c1ca7f31099379c706e
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-05 10:36:32 +0300

    Update InfoReaderImpl.java

--- src/main/java/by/epam/task1/reader/impl/InfoReaderImpl.java
+++ src/main/java/by/epam/task1/reader/impl/InfoReaderImpl.java
@@ -1,8 +1,8 @@
 package by.epam.task1.reader.impl;
 
-import by.epam.task1.exception.CustomException;
+import by.epam.task1.exception.CustomArrayException;
 import by.epam.task1.reader.InfoReader;
-import by.epam.task1.validator.StringValidator;
+import by.epam.task1.validator.ArrayValidator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,22 +15,22 @@ public class InfoReaderImpl implements InfoReader {
     static Logger logger = LogManager.getLogger();
 
     @Override
-    public String readLine(String input) throws CustomException {
-        StringValidator validator = new StringValidator();
+    public String readLine(String input) throws CustomArrayException {
+        ArrayValidator validator = new ArrayValidator();
         String currentLine = "";
 
         try (Scanner scanner = new Scanner(new FileReader(input))) {
             do {
                 currentLine = scanner.nextLine();
                 currentLine.trim();
-            } while (scanner.hasNextLine() && (!validator.validateString(currentLine)));
+            } while (scanner.hasNextLine() && (!validator.isValidString(currentLine)));
             if (currentLine == null) {
                 logger.log(Level.ERROR, "There is no correct line in the source " + input);
-                throw new CustomException("There is no correct line in the source" + input);
+                throw new CustomArrayException("There is no correct line in the source" + input);
             }
         } catch (FileNotFoundException e) {
             logger.log(Level.ERROR, "The data source " + input + " was not found", e);
-            throw new CustomException();
+            throw new CustomArrayException("The data source " + input + "was not found", e);
         }
         return currentLine;
     }
