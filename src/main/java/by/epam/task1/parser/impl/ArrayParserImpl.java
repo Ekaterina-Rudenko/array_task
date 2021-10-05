309f007591953c02a349814fa2ee4a2103996f30
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-05 10:36:35 +0300

    Update ArrayParserImpl.java

--- src/main/java/by/epam/task1/parser/impl/ArrayParserImpl.java
+++ src/main/java/by/epam/task1/parser/impl/ArrayParserImpl.java
@@ -1,15 +1,30 @@
 package by.epam.task1.parser.impl;
 
+import by.epam.task1.exception.CustomArrayException;
 import by.epam.task1.parser.ArrayParser;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ArrayParserImpl implements ArrayParser {
-    public static final String REGEX_DELIMETR_COMMA = ",";
+    static final Logger logger = LogManager.getLogger();
+    public static final String REGEX_DELIMITER_COMMA = "[,]\\s*";
+
     @Override
-    public int[] parseStringToIntegerArray(String correctLine) {
-        String[] stringArray = correctLine.split(REGEX_DELIMETR_COMMA);
+    public int[] parseStringToIntegerArray(String parseString) throws CustomArrayException {
+        if (parseString == null) {
+            logger.log(Level.ERROR, "Input string for parsing is null");
+            throw new CustomArrayException("Input string is null");
+        }
+        String[] stringArray = parseString.split(REGEX_DELIMITER_COMMA);
         int[] numberArray = new int[stringArray.length];
-        for (int i = 0; i < stringArray.length; i++) {
-            numberArray[i] = Integer.parseInt(stringArray[i]);
+        try {
+            for (int i = 0; i < stringArray.length; i++) {
+                numberArray[i] = Integer.parseInt(stringArray[i]);
+            }
+        } catch (NumberFormatException e) {
+            logger.log(Level.ERROR, "The String " + parseString + " cannot be parsed", e);
+            throw new SecurityException("The String " + parseString + " cannot be parsed", e);
         }
         return numberArray;
     }
