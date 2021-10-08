4564c2a18fc62b0e708a35a8eea10451f34fa64a
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:53:12 +0300

    Create ArrayValidatorImplTest.java

---
+++ src/test.by.epam.task1/java/test/by/epam/task1/validator/ArrayValidatorImplTest.java
@@ -0,0 +1,46 @@
+package test.by.epam.task1.validator;
+
+import by.epam.task1.validator.ArrayValidator;
+import by.epam.task1.validator.impl.ArrayValidatorImpl;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ArrayValidatorImplTest {
+    ArrayValidator validator;
+
+    @BeforeMethod
+    public void setUp() {
+        validator = new ArrayValidatorImpl();
+    }
+
+    @Test
+    public void validateStringTest() {
+        boolean actual = validator.isValidString("12, -6, 59, 37");
+        assertEquals(actual, true);
+    }
+
+    @Test
+    public void validateStringTestNegative() {
+        boolean actual = validator.isValidString("45,dfr,-6,54,32,");
+        assertNotEquals(actual, true);
+    }
+
+    @Test
+    public void validateStringWithoutSpacesTest() {
+        boolean actual = validator.isValidString("45,-6,54,32");
+        assertEquals(actual, true);
+    }
+    @Test
+    public void validateStringNegativeWithoutCommaTest() {
+        boolean actual = validator.isValidString("45 -6 54 32");
+        assertNotEquals(actual, true);
+    }
+    @Test
+    public void validateStringEmptyTest(){
+        boolean actual = validator.isValidString("");
+        assertNotEquals(actual, true);
+    }
+
+}
