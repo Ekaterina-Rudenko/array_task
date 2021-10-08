7679cadbcd10b2f75e0a74f3599af14f3b0e97ab
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:59:48 +0300

    Update ArrayCreatorImplTest.java

--- src/test.by.epam.task1/java/test/by/epam/task1/creator/ArrayCreatorImplTest.java
+++ src/test.by.epam.task1/java/test/by/epam/task1/creator/ArrayCreatorImplTest.java
@@ -18,21 +18,15 @@ public class ArrayCreatorImplTest {
     public void setUp() {
         creator = new ArrayCreatorImpl();
     }
-
     @Test
     public void createCustomArrayTest() throws CustomArrayException {
         CustomArray actual = creator.createCustomArray(new int[]{23, 35, 26});
         CustomArray expected = new CustomArray(new int[]{23, 35, 26});
-        assertEquals(actual, expected);
+        assertEquals(actual, expected, "Test failed, CustomArray was not created");
     }
     @Test(expectedExceptions = CustomArrayException.class)
     public void createCustomArrayNegativeTest() throws CustomArrayException {
         int[] array = null;
-        CustomArray actual = creator.createCustomArray(array);
-    }
-    @Test(expectedExceptions = CustomArrayException.class)
-    public void createCustomArrayNegativeLengthTest() throws CustomArrayException {
-        int[] array = {};
         CustomArray actual = creator.createCustomArray(array);
     }
 
