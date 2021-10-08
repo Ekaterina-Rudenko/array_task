c9e9c6e977da26108b72339f915b180521ecf965
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:52:22 +0300

    Update ArrayParser.java

--- src/main/java/by/epam/task1/parser/ArrayParser.java
+++ src/main/java/by/epam/task1/parser/ArrayParser.java
@@ -3,5 +3,5 @@ package by.epam.task1.parser;
 import by.epam.task1.exception.CustomArrayException;
 
 public interface ArrayParser {
-    public int[] parseStringToIntegerArray(String correctLine) throws CustomArrayException;
+    int[] parseToIntegerArray(String stringToParse) throws CustomArrayException;
 }
