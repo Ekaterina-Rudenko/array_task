cee34ef81a23aea65023795dff6c774c4559c6bb
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:53:01 +0300

    Create ArrayCreatorImplTest.java

---
+++ src/test.by.epam.task1/java/test/by/epam/task1/creator/ArrayCreatorImplTest.java
@@ -0,0 +1,39 @@
+package test.by.epam.task1.creator;
+
+import by.epam.task1.creator.ArrayCreator;
+import by.epam.task1.creator.impl.ArrayCreatorImpl;
+import by.epam.task1.entity.CustomArray;
+
+import by.epam.task1.exception.CustomArrayException;
+import by.epam.task1.parser.ArrayParser;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ArrayCreatorImplTest {
+    ArrayCreator creator;
+
+    @BeforeMethod
+    public void setUp() {
+        creator = new ArrayCreatorImpl();
+    }
+
+    @Test
+    public void createCustomArrayTest() throws CustomArrayException {
+        CustomArray actual = creator.createCustomArray(new int[]{23, 35, 26});
+        CustomArray expected = new CustomArray(new int[]{23, 35, 26});
+        assertEquals(actual, expected);
+    }
+    @Test(expectedExceptions = CustomArrayException.class)
+    public void createCustomArrayNegativeTest() throws CustomArrayException {
+        int[] array = null;
+        CustomArray actual = creator.createCustomArray(array);
+    }
+    @Test(expectedExceptions = CustomArrayException.class)
+    public void createCustomArrayNegativeLengthTest() throws CustomArrayException {
+        int[] array = {};
+        CustomArray actual = creator.createCustomArray(array);
+    }
+
+}
