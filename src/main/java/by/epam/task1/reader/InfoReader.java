97a4ba6a67c6192532af7e0018f312ebefc2ef8f
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:52:32 +0300

    Update InfoReader.java

--- src/main/java/by/epam/task1/reader/InfoReader.java
+++ src/main/java/by/epam/task1/reader/InfoReader.java
@@ -3,5 +3,5 @@ package by.epam.task1.reader;
 import by.epam.task1.exception.CustomArrayException;
 
 public interface InfoReader {
-    public String readLine(String input) throws CustomArrayException;
+    String readLine(String path) throws CustomArrayException;
 }
