186a4246d035accc6fb7a8c504357ca65067b9d9
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:52:13 +0300

    Update ArrayCreatorImpl.java

--- src/main/java/by/epam/task1/creator/impl/ArrayCreatorImpl.java
+++ src/main/java/by/epam/task1/creator/impl/ArrayCreatorImpl.java
@@ -3,22 +3,20 @@ package by.epam.task1.creator.impl;
 import by.epam.task1.creator.ArrayCreator;
 import by.epam.task1.entity.CustomArray;
 import by.epam.task1.exception.CustomArrayException;
-import by.epam.task1.validator.ArrayValidator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ArrayCreatorImpl implements ArrayCreator {
     static final Logger logger = LogManager.getLogger();
-    ArrayValidator validator = new ArrayValidator();
 
     @Override
     public CustomArray createCustomArray(int[] numberArray) throws CustomArrayException {
-        if (validator.isNonEmpty(numberArray)) {
+        if (numberArray != null) {
             return new CustomArray(numberArray);
-      } else {
-            logger.log(Level.ERROR, "The argument array is empty");
-            throw new CustomArrayException("The array is empty");
+        } else {
+            logger.log(Level.ERROR, "The argument array is empty, new object will not be created");
+            throw new CustomArrayException("The array is empty, new object will not be created");
         }
     }
 }
