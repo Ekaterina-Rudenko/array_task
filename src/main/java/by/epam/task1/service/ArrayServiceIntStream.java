e2b035096f1204eb069dc36b8ca99664afdb7aa2
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-08 09:52:38 +0300

    Update ArrayServiceIntStream.java

--- src/main/java/by/epam/task1/service/ArrayServiceIntStream.java
+++ src/main/java/by/epam/task1/service/ArrayServiceIntStream.java
@@ -3,12 +3,16 @@ package by.epam.task1.service;
 import by.epam.task1.entity.CustomArray;
 import by.epam.task1.exception.CustomArrayException;
 
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
 public interface ArrayServiceIntStream {
-    public int findMaxIntStream(CustomArray customArray);
-    public int findMinIntStream(CustomArray customArray);
-    public int findSumIntStream(CustomArray customArray) throws CustomArrayException;
-    public double findAverageIntStream(CustomArray customArray) throws CustomArrayException;
-    public void swapNegativeIntStream(CustomArray customArray);
-    public long countPositiveIntStream(CustomArray customArray);
-    public long countNegativeIntStream(CustomArray customArray);
+    OptionalInt findMaxIntStream(CustomArray customArray);
+    OptionalInt findMinIntStream(CustomArray customArray);
+    int findSumIntStream(CustomArray customArray) throws CustomArrayException;
+    OptionalDouble findAverageIntStream(CustomArray customArray) throws CustomArrayException;
+    void swapNegativeIntStream(CustomArray customArray);
+    long countPositiveIntStream(CustomArray customArray);
+    long countNegativeIntStream(CustomArray customArray);
+    void sortCustomArrayIntStream(CustomArray customArray);
 }
