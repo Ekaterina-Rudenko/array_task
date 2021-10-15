35e0ea0fc244a7823b7ce1f1d67f84ee1d0c5d0c
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:21 +0300

    Update ArrayService.java

--- src/main/java/by/epam/task1/service/ArrayService.java
+++ src/main/java/by/epam/task1/service/ArrayService.java
@@ -3,12 +3,15 @@ package by.epam.task1.service;
 import by.epam.task1.entity.CustomArray;
 import by.epam.task1.exception.CustomArrayException;
 
-public interface ArrayService  {
-    int findMax(CustomArray customArray);
-    int findMin(CustomArray customArray);
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
+public interface ArrayService {
+    OptionalInt findMax(CustomArray customArray) throws CustomArrayException;
+    OptionalInt findMin(CustomArray customArray) throws CustomArrayException;
     int findSumOfElements(CustomArray customArray) throws CustomArrayException;
-    double findAverage(CustomArray customArray) throws CustomArrayException;
-    void swapNegative(CustomArray customArray);
+    OptionalDouble findAverage(CustomArray customArray) throws CustomArrayException;
+    int[] swapNegativeElement(CustomArray customArray, int replaceValue);
     int countPositive(CustomArray customArray);
     int countNegative(CustomArray customArray);
     int[] quickSort(CustomArray customArray);
