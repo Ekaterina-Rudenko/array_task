c49bcb9e72325045e6e84154a09761c719282e4c
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:56 +0300

    Update ArrayParserImplTest.java

--- src/test.by.epam.task1/java/test/by/epam/task1/parser/ArrayParserImplTest.java
+++ src/test.by.epam.task1/java/test/by/epam/task1/parser/ArrayParserImplTest.java
@@ -1,38 +1,49 @@
 package test.by.epam.task1.parser;
 
-import by.epam.task1.exception.CustomArrayException;
 import by.epam.task1.parser.ArrayParser;
 import by.epam.task1.parser.impl.ArrayParserImpl;
-import by.epam.task1.reader.impl.InfoReaderImpl;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.testng.Assert.*;
 
 public class ArrayParserImplTest {
     ArrayParser parser;
+
     @BeforeMethod
     public void setUp() {
         parser = new ArrayParserImpl();
     }
+
     @Test
-    public void parseToInt() throws CustomArrayException {
-        int[] actual = parser.parseToIntegerArray("2, -5, 4, 3, 8");
-        int[] expected = {2, -5, 4, 3, 8};
+    public void parseToInt() {
+        List<Integer> actual = parser.parseStringToIntegerList(Optional.of("2, -5, 4, 3, 8"));
+        List<Integer> expected = List.of(2, -5, 4, 3, 8);
         assertEquals(actual, expected);
     }
 
-    @Test(expectedExceptions = CustomArrayException.class)
-    public void parseToIntegerArrayExceptionTest() throws CustomArrayException {
-        int[] actual = parser.parseToIntegerArray("2.3, 4.5, 6.7");
+    @Test
+    public void parseToIntegerArrayExceptionTest() {
+        List<Integer> actual = parser.parseStringToIntegerList(Optional.of("2.3, 4.5, 6.7"));
+        List<Integer> expected = List.of();
+        assertEquals(actual, expected);
     }
-    @Test (expectedExceptions = CustomArrayException.class)
-    public void parseToIntegerArrayNegativeTest() throws CustomArrayException {
-        int[] actual = parser.parseToIntegerArray("2 -5 4 3 8");
+
+    @Test
+    public void parseToIntArrayWrongFormatTest() {
+        List<Integer> actual = parser.parseStringToIntegerList(Optional.of("2 -5 4 3 8"));
+        List<Integer> expected = List.of();
+        assertEquals(actual, expected);
     }
-    @Test (expectedExceptions = CustomArrayException.class)
-    public void parseToIntegerArrayEmptyTest() throws CustomArrayException {
-        String toParse = null;
-        int[] actual = parser.parseToIntegerArray(toParse);
+
+    @Test
+    public void parseToIntegerArrayEmptyTest() {
+        List<Integer> actual = parser.parseStringToIntegerList(Optional.empty());
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(actual, expected);
     }
 }
