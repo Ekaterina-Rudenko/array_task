3b44209edc7c68a79956ba300d5d5d2378b1c279
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:44 +0300

    Update ArrayValidatorImpl.java

--- src/main/java/by/epam/task1/validator/impl/ArrayValidatorImpl.java
+++ src/main/java/by/epam/task1/validator/impl/ArrayValidatorImpl.java
@@ -2,17 +2,13 @@ package by.epam.task1.validator.impl;
 
 import by.epam.task1.validator.ArrayValidator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class ArrayValidatorImpl implements ArrayValidator {
     public static final String REGEX_IS_NUMERICAL_STRING = "^-?\\d+([,]\\s?(-?\\d+))*$";
+
     @Override
-    public boolean isValidString(String inputLine) {
+    public boolean validateString(String inputLine) {
         boolean isValidLine = false;
-        Pattern pattern = Pattern.compile(REGEX_IS_NUMERICAL_STRING);
-        Matcher matcher = pattern.matcher(inputLine);
-        if (matcher.matches()) {
+        if (inputLine != null && inputLine.matches(REGEX_IS_NUMERICAL_STRING)) {
             isValidLine = true;
         }
         return isValidLine;
