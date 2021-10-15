334b0a88e527f83939150aeb3a815b4b631447c0
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:11 +0300

    Update ArrayParserImpl.java

--- src/main/java/by/epam/task1/parser/impl/ArrayParserImpl.java
+++ src/main/java/by/epam/task1/parser/impl/ArrayParserImpl.java
@@ -1,31 +1,33 @@
 package by.epam.task1.parser.impl;
 
-import by.epam.task1.exception.CustomArrayException;
 import by.epam.task1.parser.ArrayParser;
+import by.epam.task1.validator.ArrayValidator;
+import by.epam.task1.validator.impl.ArrayValidatorImpl;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class ArrayParserImpl implements ArrayParser {
     static final Logger logger = LogManager.getLogger();
     public static final String REGEX_DELIMITER_COMMA = "[,]\\s*";
 
     @Override
-    public int[] parseToIntegerArray(String stringToParse) throws CustomArrayException {
-        if (stringToParse == null) {
-            logger.log(Level.ERROR, "String for parsing is empty");
-            throw new CustomArrayException("Input string is null");
-        }
-        String[] stringArray = stringToParse.split(REGEX_DELIMITER_COMMA);
-        int[] numberArray = new int[stringArray.length];
-        try {
-            for (int i = 0; i < numberArray.length; i++) {
-                numberArray[i] = Integer.parseInt(stringArray[i]);
+    public List<Integer> parseStringToIntegerList(Optional<String> stringToParse) {
+        ArrayValidator validator = new ArrayValidatorImpl();
+        List<Integer> numberList = new ArrayList<>();
+        if (stringToParse.isPresent() && validator.validateString(stringToParse.get())) {
+            String[] stringArray = stringToParse.get().split(REGEX_DELIMITER_COMMA);
+            for (String number : stringArray) {
+                numberList.add(Integer.parseInt(number));
             }
-        } catch (NumberFormatException e) {
-            logger.log(Level.ERROR, "The String " + stringToParse + " cannot be parsed", e);
-            throw new CustomArrayException("The String " + stringToParse + " cannot be parsed", e);
+            logger.log(Level.INFO, "Parsing is completed");
         }
-        return numberArray;
+        return numberList;
     }
 }
+
+
