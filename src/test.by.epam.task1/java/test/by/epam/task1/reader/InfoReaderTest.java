c52ae5f4f49115cf1ad4a3c022c06f1effb810af
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:14:44 +0300

    Update InfoReaderTest.java

--- src/test.by.epam.task1/java/test/by/epam/task1/reader/InfoReaderTest.java
+++ src/test.by.epam.task1/java/test/by/epam/task1/reader/InfoReaderTest.java
@@ -6,7 +6,9 @@ import by.epam.task1.reader.impl.InfoReaderImpl;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.util.Optional;
+
+import static org.testng.Assert.assertEquals;
 
 public class InfoReaderTest {
 
@@ -20,25 +22,27 @@ public class InfoReaderTest {
     @Test
     public void readLineTest() throws CustomArrayException {
         String path = "testdata/test.txt";
-        String actual = reader.readLine(path);
-        String expected = "45,-3,34,56,22,56,-100,35";
+        Optional<String> actual = reader.readLine(path);
+        Optional<String> expected = Optional.of("45,-3,34,56,22,56,-100,35");
         assertEquals(actual, expected);
     }
 
     @Test(expectedExceptions = CustomArrayException.class, expectedExceptionsMessageRegExp = "The source tesdata/noFile.txt was not found")
-    public void readNoFileTest() throws CustomArrayException {
+    public void readLineNoFileTest() throws CustomArrayException {
         reader.readLine("tesdata/noFile.txt");
     }
 
-    @Test(expectedExceptions = CustomArrayException.class, expectedExceptionsMessageRegExp = "The file testdata/empty.txt is empty or does not contain valid lines")
-    public void readEmptyFileTest() throws CustomArrayException {
-        reader.readLine("testdata/empty.txt");
+    @Test
+    public void readLineEmptyFileTest() throws CustomArrayException {
+        Optional<String> actual = reader.readLine("testdata/empty.txt");
+        Optional<String> expected = Optional.empty();
+        assertEquals(actual, expected);
     }
 
-    @Test(expectedExceptions = CustomArrayException.class, expectedExceptionsMessageRegExp = "The file testdata/wrong.txt is empty or does not contain valid lines")
-    public void readWrongLineTest() throws CustomArrayException {
-        String pathEmptyFile = "testdata/wrong.txt";
-        String actual = reader.readLine(pathEmptyFile);
+    @Test
+    public void readLineAllWrongLinesTest() throws CustomArrayException {
+        Optional<String> actual = reader.readLine("testdata/wrong.txt");
+        Optional<String> expected = Optional.empty();
+        assertEquals(actual, expected);
     }
-
 }
