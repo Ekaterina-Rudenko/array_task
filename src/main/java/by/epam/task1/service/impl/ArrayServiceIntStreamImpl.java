fdca8a7325726191de28893bbc861166ca8a36d1
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:37 +0300

    Update ArrayServiceIntStreamImpl.java

--- src/main/java/by/epam/task1/service/impl/ArrayServiceIntStreamImpl.java
+++ src/main/java/by/epam/task1/service/impl/ArrayServiceIntStreamImpl.java
@@ -1,44 +1,48 @@
 package by.epam.task1.service.impl;
 
 import by.epam.task1.entity.CustomArray;
-import by.epam.task1.exception.CustomArrayException;
-import by.epam.task1.service.ArrayServiceIntStream;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class ArrayServiceIntStreamImpl implements by.epam.task1.service.ArrayServiceIntStream {
     static final Logger logger = LogManager.getLogger();
+
     @Override
     public OptionalInt findMaxIntStream(CustomArray customArray) {
-        OptionalInt max = IntStream.of(customArray.getArray())
-                .max();
+        OptionalInt max = OptionalInt.empty();
+        if (customArray != null) {
+            max = IntStream.of(customArray.getArray())
+                    .max();
+        }
         logger.log(Level.INFO, "The maximum by IntStream is " + max);
         return max;
     }
+
     @Override
     public OptionalInt findMinIntStream(CustomArray customArray) {
-        OptionalInt min = IntStream.of(customArray.getArray()).min();
+        OptionalInt min = OptionalInt.empty();
+        if (customArray != null) {
+            min = IntStream.of(customArray.getArray()).min();
+        }
         logger.log(Level.INFO, "The minimum by IntStream is " + min);
         return min;
     }
+
     @Override
-    public int findSumIntStream(CustomArray customArray) throws CustomArrayException {
+    public int findSumIntStream(CustomArray customArray) {
         int sum = IntStream.of(customArray.getArray()).sum();
         logger.log(Level.INFO, "The sum by IntStream is " + sum);
         return sum;
     }
 
     @Override
-    public OptionalDouble findAverageIntStream(CustomArray customArray) throws CustomArrayException {
+    public OptionalDouble findAverageIntStream(CustomArray customArray) {
         OptionalDouble average = IntStream.of(customArray.getArray())
                 .average();
         logger.log(Level.INFO, "The average by IntStream is " + average);
@@ -46,13 +50,13 @@ public class ArrayServiceIntStreamImpl implements by.epam.task1.service.ArraySer
     }
 
     @Override
-    public void swapNegativeIntStream(CustomArray customArray) {
-        final int SWAP_NEGATIVE_VALUE = 999;
-        int [] array = customArray.getArray();
+    public int[] swapNegativeIntStream(CustomArray customArray, int replaceValue) {
+        int[] array = customArray.getArray();
         array = IntStream.of(array)
-                .map( i -> i < 0 ? SWAP_NEGATIVE_VALUE : i)
+                .map(i -> i < 0 ? replaceValue : i)
                 .toArray();
         logger.log(Level.INFO, "The swapped array by IntStream is " + Arrays.toString(array));
+        return array;
     }
 
     @Override
@@ -72,9 +76,11 @@ public class ArrayServiceIntStreamImpl implements by.epam.task1.service.ArraySer
         logger.log(Level.INFO, "The number of negative elements by IntStream is " + counter);
         return counter;
     }
+
     @Override
-    public void sortCustomArrayIntStream(CustomArray customArray){
-       int[] array = IntStream.of(customArray.getArray()).sorted().toArray();
-       logger.log(Level.INFO, "The sorted array by IntStream is " + Arrays.toString(array));
+    public int[] sortCustomArrayIntStream(CustomArray customArray) {
+        int[] array = IntStream.of(customArray.getArray()).sorted().toArray();
+        logger.log(Level.INFO, "The sorted array by IntStream is " + Arrays.toString(array));
+        return array;
     }
 }
