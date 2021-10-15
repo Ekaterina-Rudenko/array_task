104e61982df7400031886eadb3ad1e245137c73a
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-15 03:16:53 +0300

    Update ArrayCreatorImplTest.java

--- src/test.by.epam.task1/java/test/by/epam/task1/creator/ArrayCreatorImplTest.java
+++ src/test.by.epam.task1/java/test/by/epam/task1/creator/ArrayCreatorImplTest.java
@@ -3,11 +3,11 @@ package test.by.epam.task1.creator;
 import by.epam.task1.creator.ArrayCreator;
 import by.epam.task1.creator.impl.ArrayCreatorImpl;
 import by.epam.task1.entity.CustomArray;
-
 import by.epam.task1.exception.CustomArrayException;
-import by.epam.task1.parser.ArrayParser;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.testng.Assert.*;
 
@@ -18,16 +18,29 @@ public class ArrayCreatorImplTest {
     public void setUp() {
         creator = new ArrayCreatorImpl();
     }
+
     @Test
     public void createCustomArrayTest() throws CustomArrayException {
-        CustomArray actual = creator.createCustomArray(new int[]{23, 35, 26});
+        CustomArray actual = creator.createCustomArray(List.of(23, 35, 26));
         CustomArray expected = new CustomArray(new int[]{23, 35, 26});
-        assertEquals(actual, expected, "Test failed, CustomArray was not created");
-    }
-    @Test(expectedExceptions = CustomArrayException.class)
-    public void createCustomArrayNegativeTest() throws CustomArrayException {
-        int[] array = null;
-        CustomArray actual = creator.createCustomArray(array);
+        assertEquals(actual, expected);
     }
 
+    @Test
+    public void createCustomArrayNegativeTest() throws CustomArrayException {
+        CustomArray actual = creator.createCustomArray(List.of(0, 0, 0, 0, 23, 12));
+        CustomArray expected = new CustomArray(new int[]{23, 12});
+        assertNotEquals(actual, expected);
+    }
+
+    @Test(expectedExceptions = CustomArrayException.class)
+    public void createCustomArrayExceptionTest() throws CustomArrayException {
+        List<Integer> numberListTest = null;
+        CustomArray actual = creator.createCustomArray(numberListTest);
+    }
+
+    @Test(expectedExceptions = CustomArrayException.class)
+    public void createCustomArrayEmptyExceptionTest() throws CustomArrayException {
+        CustomArray actual = creator.createCustomArray(List.of());
+    }
 }
